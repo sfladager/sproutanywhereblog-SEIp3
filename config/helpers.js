7d99cd7f0fc2ce509e23d18fc4b306ceff3c726e
dimcrack <gael.duchesne@hotmail.fr>
2022-11-17 15:45:29 +0000

    added secureRoute and updateProfile

--- config/helpers.js
+++ config/helpers.js
@@ -1,5 +1,6 @@
 import { NotFound, Unauthorised } from './errors.js'
 import { CastError } from 'mongoose'
+import User from '../models/user.js'
 
 // We will return specific error messages for the user so we create a sendErrors function that we will reuse in our controllers.
 export const sendErrors = (res, err) => {
@@ -17,5 +18,30 @@ export const sendErrors = (res, err) => {
     return res.status(401).json({ message: err.message })
   } else {
     return res.status(500).json({ message: err.message })
+  }
+}
+
+export const findAllUsers = async (req, res) => {
+  try {
+    // const { id } = req.params
+    const users = await User.find()
+    if (!users) throw new NotFound('User not found')
+    console.log(users)
+    res.json(users)
+    return users
+  } catch (err) {
+    sendErrors(res, err)
+  }
+}
+
+export const findSingleUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (!user) throw new NotFound('User not found')
+    console.log(user)
+    return user
+  } catch (err) {
+    sendErrors(res, err)
   }
 }
