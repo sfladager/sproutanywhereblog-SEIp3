daf7de500f8a905303a8563b2f81bd2150f98f61
Shawn Fladager <shawnfladager@gmail.com>
2022-11-24 20:23:50 +0100

    small changes

--- client/src/components/pages/plants/PlantSingle.js
+++ client/src/components/pages/plants/PlantSingle.js
@@ -1,112 +1,198 @@
-// import { useState, useEffect } from 'react'
-// import { Link, useParams, useNavigate } from 'react-router-dom'
-// import axios from 'axios'
-// import { getToken, isOwner } from '../../../helpers/auth'
+import { useState, useEffect } from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-// // Chakra imports
-// import { Container, Box, SimpleGrid, Flex, Spacer, Text, Card, CardBody, Image, Heading, Button, ButtonGroup } from '@chakra-ui/react'
-// import { FaAmazon } from 'react-icons/fa'
-// import lightIcon from '../../../assets/light-icon.webp'
-// import waterIcon from '../../../assets/water-icon.webp'
-// import tempIcon from '../../../assets/temp-icon.webp'
-// import humidIcon from '../../../assets/humid-icon.webp'
-// import timeIcon from '../../../assets/time-icon.webp'
-// import petIcon from '../../../assets/pet-icon.webp'
+import { getToken, isOwner } from '../../../helpers/auth'
+
+// Chakra imports
+import { Container, Box, SimpleGrid, Flex, Spacer, Text, Card, CardBody, Image, Heading, Button, ButtonGroup } from '@chakra-ui/react'
+import { FaAmazon } from 'react-icons/fa'
+import lightIcon from '../../../assets/light-icon.webp'
+import waterIcon from '../../../assets/water-icon.webp'
+import tempIcon from '../../../assets/temp-icon.webp'
+import humidIcon from '../../../assets/humid-icon.webp'
+import timeIcon from '../../../assets/time-icon.webp'
+import petIcon from '../../../assets/pet-icon.webp'
 
 
-// const PlantsIndex = () => {
+const PlantsIndex = () => {
 
-//   const [plant, setPlant] = useState(null)
-//   const [error, setError] = useState(false)
-//   const [reviewField, setReviewField] = useState({
-//     text: '',
-//   })
+  const [plant, setPlant] = useState(null)
+  const [error, setError] = useState(false)
+  const [reviewField, setReviewField] = useState({
+    text: '',
+  })
 
-//   const { id } = useParams()
-//   const navigate = useNavigate()
+  const { id } = useParams()
+  const navigate = useNavigate()
 
-//   useEffect(() => {
-//     const getPlant = async () => {
-//       try {
-//         // console.log(plant)
-//         const { data } = await axios.get(`/api/plants/${id}`)
-//         // console.log(data)
-//         setPlant(data)
-//       } catch (err) {
-//         setError(true)
-//       }
-//     }
-//     getPlant()
-//   }, [id])
 
-//   // useEffect(() => {
-//   //   console.log('plant: ' + plant)
-//   // }, [plant])
-//   // const { name, thumbnail, mainDescription, lightDescription, waterDescription, tempDescription, humidityDescription, heightDescription, toxicityDescription, reviews, owner } = plant
+  useEffect(() => {
+    const getPlant = async () => {
+      try {
+        const { data } = await axios.get(`/api/plants/${id}`)
+        console.log(data)
+        setPlant(data)
+      } catch (err) {
+        setError(true)
+      }
+    }
+    getPlant()
+  }, [id])
 
-//   // useEffect(() => {
-//   //   setPlant({ ...plant, reviews: reviewField.text })
-//   //   console.log(plant)
-//   // }, [reviewField])
+  const deletePlant = async (e) => {
+    try {
+      await axios.delete(`/api/plants/${id}`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+      navigate('/plants')
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-//   const handleChange = (e) => {
-//     const updatedReviewField = {
-//       ...reviewField,
-//       [e.target.name]: e.target.value,
-//     }
-//     setReviewField(updatedReviewField)
-//     if (error) setError('')
-//   }
+  const amazonClick = () => {
+    window.open(
+      `https://www.amazon.co.uk/s?k=${plant.name}`
+    )
+  }
 
-//   const handleClick = async (e) => {
-//     try {
-//       console.log(plant)
-//       const { data } = await axios.post(`/api/plants/${id}/review`, { ...plant, reviews: reviewField }, {
-//       }, [id])
+  const handleChange = (e) => {
+    const updatedReviewField = {
+      ...reviewField,
+      [e.target.name]: e.target.value,
+    }
+    setReviewField(updatedReviewField)
+    if (error) setError('')
+  }
 
-//       // const { name, thumbnail, mainDescription, lightDescription, waterDescription, tempDescription, humidityDescription, heightDescription, toxicityDescription } = plant
+  const handleClick = async (e) => {
+    try {
+      console.log(id)
+      const { data } = await axios.post(`/api/plants/${id}/review`, reviewField, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+      setPlant(data)
+      setReviewField({
+        text: '',
+      })
+      console.log('review SUCCESS ->', data)
+    } catch (err) {
+      console.log('review FAIL ->', err)
+      setError(err.response.data)
+    }
+  }
 
-//       // useEffect(() => {
-//       //   console.log(plant)
-//       //   console.log(plant.owner)
-//       // }, [plant])
 
-//       const deletePlant = async (e) => {
-//         try {
-//           await axios.delete(`/api/plants/${id}`, {
-//             headers: {
-//               Authorization: `Bearer ${getToken()}`,
-//             },
-//           })
-//           console.log('EDIT SUCCESS ->', data)
-//         } catch (err) {
-//           console.log('EDIT FAIL ->', err)
-//           setError(err.response.data)
-//         }
-//       }
-//       navigate('/plants')
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
+  return (
+    <>
+      <main className="plant-single">
+        <Flex>
 
-//   const amazonClick = () => {
-//     window.open(
-//       `https://www.amazon.co.uk/s?k=${name}`
-//     )
-//   }
-//     <>
-//               <input
-//                 type="text"
-//                 name="text"
-//                 onChange={handleChange}
-//                 // value={reviewField.text}
-//                 placeholder="Review"
-//               />
-//               <button className="btn-post" onClick={handleClick}>Post review</button>
-//             </main >
-//     </>
-//         )
-// }
+        </Flex>
+        <Container className="plant-container">
+          {plant ?
+            <>
+              <h1><strong>{plant.name}</strong></h1>
+              <img src={plant.thumbnail} alt={plant.name} />
+              <p>{plant.mainDescription}</p>
+              <Button className='amazon-btn' bg="rgba(255,153,0,1)"
+                _hover={{ backgroundColor: 'rgba(255,130,0,1)' }}
+                boxShadow="xl"
+                leftIcon={<FaAmazon />}
+                onClick={amazonClick}>Buy on Amazon</Button>
+              <Flex className="plant-info-guide-title-container">
+                <p className="plant-info-guide-title">{plant.name} Care &amp; Info Guide</p>
+              </Flex>
+              <Flex className="plant-info-container">
+                <Flex className="plant-info light-info">
+                  <img src={lightIcon} />
+                  <p className="plant-info-subtitle">Light</p>
+                  <p>{plant.lightDescription}</p>
+                </Flex>
+                <Flex className="plant-info water-info">
+                  <img src={waterIcon} />
+                  <p className="plant-info-subtitle">Watering</p>
+                  <p>{<p>{plant.waterDescription}</p>}</p>
+                </Flex>
+                <Flex className="plant-info temp-info">
+                  <img src={tempIcon} />
+                  <p className="plant-info-subtitle">Temperature</p>
+                  <p>{plant.tempDescription}</p>
+                </Flex>
+                <Flex className="plant-info humid-info">
+                  <img src={humidIcon} />
+                  <p className="plant-info-subtitle">Humidity</p>
+                  <p>{plant.humidityDescription}</p>
+                </Flex>
+                <Flex className="plant-info growth-info">
+                  <img src={timeIcon} />
+                  <p className="plant-info-subtitle">Height &amp; Growth Rate</p>
+                  <p>{plant.heightDescription}</p>
+                </Flex>
+                <Flex className="plant-info toxic-info">
+                  <img src={petIcon} />
+                  <p className="plant-info-subtitle">Toxicity</p>
+                  <p>{plant.toxicityDescription}</p>
+                </Flex>
+                <Box>
+                  {/* This checks if the owner of the plant is viewing the page, and if so, displays the edit/delete buttons */}
+                  {
+                    isOwner(plant.owner) &&
+                    <ButtonGroup gap='2'>
+                      <Link to={`/plants/${id}/edit`}><Button className="btn-green">Edit</Button></Link>
+                      <Button onClick={deletePlant} colorScheme='red'>Delete</Button>
+                    </ButtonGroup>
+                  }
+                </Box>
+                <Flex className="plant-info review-info">
+                  <p className="review-section">Any comments ?</p>
+                  {plant.reviews.length > 0 &&
+                    plant.reviews.map(review => {
+                      return (
+                        <div key={review._id} className='review-flex'>
+                          <div className='review-details'>
+                            <p><span>{review.username}</span></p>
+                            <p id='review-date'>{Date().toString().split('G').slice(0, 1).join()}</p>
+                          </div>
+                          <p id='review-text'>{review.text}</p>
+                        </div>
+                      )
+                    })
+                  }
 
-// export default PlantsIndex
+                  <div className='input-review-flex'>
+
+                    <input
+                      type="text"
+                      name="text"
+                      onChange={handleChange}
+                      value={reviewField.text}
+                      placeholder="Comment"
+                    />
+                    <button className="btn-post" onClick={handleClick}>Post comment</button>
+                  </div>
+                </Flex>
+              </Flex>
+            </>
+            :
+            error ?
+              <div className="error">
+                <h2 className="error-text display-6">Something went wrong, try again later.</h2>
+              </div>
+              :
+              <h2>Loading...</h2>
+          }
+        </Container>
+      </main>
+
+    </>
+  )
+}
+
+export default PlantsIndex
+
